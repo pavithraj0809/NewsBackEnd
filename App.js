5d700ef5fdeb377c6e4b5bda59da513b7492819b
pavithraj0809 <pavithraj0809@gmail.com>
2024-06-17 13:57:52 +0530

    DB added

--- App.js
+++ App.js
@@ -13,15 +13,21 @@ app.use(cors());
 app.use(cors({orgin:"*"}))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
+    res.send(" Welcome to News WebApplication Backend")
+})
 app.use("/user",router);
 app.use("/content",newsrouter);
 
-mongoose.connect('mongodb://localhost:27017').then((req,res)=>{
+mongoose.connect('mongodb+srv://Boo:Boopathi@cluster0.krg40ao.mongodb.net/newsDB').then((req,res)=>{
     console.log("Database Connected")
 }).catch((error)=>{
     console.log("Error occured In DB");
 })
-app.listen(port,()=>{
-    console.log("Server started at port")
-})
+// app.listen(port,()=>{
+//     console.log("Server started at port")
+// })
+
+module.exports=app;
 
